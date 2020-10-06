4f882225f97485d123ebdfdbb3a6fe6291ae1111
Bachmeier <nbachmei@amazon.com>
2020-10-06 17:25:12 -0400

    add sample code to grey scale and resize

---
+++ Resize/script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+img = cv2.imread('./Resize/test.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+dim = (64,64)
+resized = cv2.resize(gray, dim, interpolation=cv2.INTER_AREA)
+plt.imshow(resized[:,::-1])
