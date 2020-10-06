c49033ac5b16be574131654c4be410c89e038a1d
Bachmeier <nbachmei@amazon.com>
2020-10-06 18:29:00 -0400

    add buildspec for codebuild

--- lambda/layers/build.bat
+++ lambda/layers/build.bat
@@ -1,9 +1,18 @@
-@RMDIR /S /Y photo-deps\*
-@DEL /Y photo-deps.zip
+@REM =================
+@REM The root folder in zip must be called "python"
+@REM =================
 
-pip install --target photo-deps matplotlib
-pip install --target photo-deps opencv-python
+@RMDIR /S /Q photo-deps\*
+@SET target=photo-deps\python
+
+@MKDIR %target%
+@DEL /Q photo-deps.zip
+
+@CALL pip install --target %target% matplotlib
+@CALL pip install --target %target% opencv-python
+
 @PUSHD photo-deps
 7z a -tzip ..\photo-deps.zip .
 @POPD
 
+aws s3 cp photo-deps.zip s3://nbachmei.personal/artifacts/
