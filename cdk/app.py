2ba0bdb6d8dc967c6b403a138664014ff63a40ee
Bachmeier <nbachmei@amazon.com>
2020-10-07 15:48:58 -0400

    seems to mostly work

--- cdk/app.py
+++ cdk/app.py
@@ -2,6 +2,7 @@
 import os.path
 from aws_cdk import (
     aws_s3 as s3,
+    aws_iam as iam,
     aws_lambda as lambda_,
     core
 )
@@ -13,18 +14,28 @@ class PhotoResizer(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.dep_layers = lambda_.LayerVersion(self,'YelpPhotoResizerDeps',
+        # Create a layer for function dependencies...
+        dependencies_layer = lambda_.LayerVersion(self,'YelpPhotoResizerDeps',
             code=lambda_.Code.from_asset(
                 os.path.join(src_root_dir, 'lambda/layers/photo-deps/')),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_8],
             description="Dependencies of lambda://yelp-photo-resize")
 
-        lambda_.Function(self,"yelp-photo-resizer",
+        # Create the lambda function itself...
+        resizer_lambda = lambda_.Function(self,"yelp-photo-resizer",
             code=lambda_.Code.from_asset(
                 os.path.join(src_root_dir, 'lambda/yelp-photo-resize/')),
             runtime= lambda_.Runtime.PYTHON_3_8,
             handler="lambda_function.lambda_handler",
-            layers=[self.dep_layers])
+            tracing= lambda_.Tracing.ACTIVE,
+            memory_size=256,
+            timeout= core.Duration.seconds(60),
+            layers=[dependencies_layer])
+
+        # Attach policies neccessary for the code to work
+        for policyName in ['AWSXrayWriteOnlyAccess','AmazonS3FullAccess','AmazonDynamoDBFullAccess']:
+            resizer_lambda.role.add_managed_policy(
+                iam.ManagedPolicy.from_aws_managed_policy_name(policyName))
 
 app = core.App()
 PhotoResizer(app, "yelp-photo-resizer")
